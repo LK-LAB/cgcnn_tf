b6e8b06c09ddb7f815c9d04b875962c92d9a250f
LK <dg981001@naver.com>
2022-02-24 11:24:29 +0900

    upload cgcnn tensorflow version

---
+++ cgcnn/data_tf.py
@@ -0,0 +1,387 @@
+from __future__ import print_function, division
+
+import csv
+import functools
+import json
+import os
+import random
+import warnings
+
+import numpy as np
+import torch
+from pymatgen.core.structure import Structure
+# from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.dataloader import default_collate
+from torch.utils.data.sampler import SubsetRandomSampler
+
+import tensorflow as tf
+#from tensorflow.data import Dataset
+import math
+
+def get_train_val_test_loader(dataset, collate_fn=default_collate,
+                              batch_size=64, train_ratio=None,
+                              val_ratio=0.1, test_ratio=0.1, return_test=False,
+                              num_workers=1, pin_memory=False, **kwargs):
+    """
+    Utility function for dividing a dataset to train, val, test datasets.
+
+    !!! The dataset needs to be shuffled before using the function !!!
+
+    Parameters
+    ----------
+    dataset: torch.utils.data.Dataset
+      The full dataset to be divided.
+    collate_fn: torch.utils.data.DataLoader
+    batch_size: int
+    train_ratio: float
+    val_ratio: float
+    test_ratio: float
+    return_test: bool
+      Whether to return the test dataset loader. If False, the last test_size
+      data will be hidden.
+    num_workers: int
+    pin_memory: bool
+
+    Returns
+    -------
+    train_loader: torch.utils.data.DataLoader
+      DataLoader that random samples the training data.
+    val_loader: torch.utils.data.DataLoader
+      DataLoader that random samples the validation data.
+    (test_loader): torch.utils.data.DataLoader
+      DataLoader that random samples the test data, returns if
+        return_test=True.
+    """
+    total_size = len(dataset)
+    if kwargs['train_size'] is None:
+        if train_ratio is None:
+            assert val_ratio + test_ratio < 1
+            train_ratio = 1 - val_ratio - test_ratio
+            print(f'[Warning] train_ratio is None, using 1 - val_ratio - '
+                  f'test_ratio = {train_ratio} as training data.')
+        else:
+            assert train_ratio + val_ratio + test_ratio <= 1
+    indices = list(range(total_size))
+    if kwargs['train_size']:
+        train_size = kwargs['train_size']
+    else:
+        train_size = int(train_ratio * total_size)
+    if kwargs['test_size']:
+        test_size = kwargs['test_size']
+    else:
+        test_size = int(test_ratio * total_size)
+    if kwargs['val_size']:
+        valid_size = kwargs['val_size']
+    else:
+        valid_size = int(val_ratio * total_size)
+    train_sampler = SubsetRandomSampler(indices[:train_size])
+    val_sampler = SubsetRandomSampler(
+        indices[-(valid_size + test_size):-test_size])
+    if return_test:
+        test_sampler = SubsetRandomSampler(indices[-test_size:])
+    train_loader = DataLoader(dataset, batch_size=batch_size,
+                              sampler=train_sampler,
+                              num_workers=num_workers,
+                              collate_fn=collate_fn, pin_memory=pin_memory)
+    val_loader = DataLoader(dataset, batch_size=batch_size,
+                            sampler=val_sampler,
+                            num_workers=num_workers,
+                            collate_fn=collate_fn, pin_memory=pin_memory)
+    if return_test:
+        test_loader = DataLoader(dataset, batch_size=batch_size,
+                                 sampler=test_sampler,
+                                 num_workers=num_workers,
+                                 collate_fn=collate_fn, pin_memory=pin_memory)
+    if return_test:
+        return train_loader, val_loader, test_loader
+    else:
+        return train_loader, val_loader
+
+
+class Dataloader(tf.keras.utils.Sequence):
+
+    def __init__(self, dataset, batch_size, shuffle=False):
+        #self.x, self.y = x_set, y_set
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle=shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        return math.ceil(len(self.dataset) / self.batch_size)
+
+		# batch 단위로 직접 묶어줘야 함
+    def __getitem__(self, idx):
+				# sampler의 역할(index를 batch_size만큼 sampling해줌)
+        indices = self.indices[idx*self.batch_size:(idx+1)*self.batch_size]
+
+        batch_dataset = [self.dataset[i] for i in indices]
+        batch_x, batch_y, batch_id = collate_pool(batch_dataset)
+        #batch_x = [self.x[i] for i in indices]
+        #batch_y = [self.y[i] for i in indices]
+        print("pass")
+        return batch_x, batch_y
+
+    # epoch이 끝날때마다 실행
+    def on_epoch_end(self):
+        self.indices = np.arange(len(self.dataset))
+        if self.shuffle == True:
+            np.random.shuffle(self.indices)
+
+
+
+def collate_pool(dataset_list):
+    """
+    Collate a list of data and return a batch for predicting crystal
+    properties.
+
+    Parameters
+    ----------
+
+    dataset_list: list of tuples for each data point.
+      (atom_fea, nbr_fea, nbr_fea_idx, target)
+
+      atom_fea: tf.Tensor shape (n_i, atom_fea_len)
+      nbr_fea: tf.Tensor shape (n_i, M, nbr_fea_len)
+      nbr_fea_idx: torch.LongTensor shape (n_i, M)
+      target: tf.Tensor shape (1, )
+      cif_id: str or int
+
+    Returns
+    -------
+    N = sum(n_i); N0 = sum(i)
+
+    batch_atom_fea: tf.Tensor shape (N, orig_atom_fea_len)
+      Atom features from atom type
+    batch_nbr_fea: tf.Tensor shape (N, M, nbr_fea_len)
+      Bond features of each atom's M neighbors
+    batch_nbr_fea_idx: torch.LongTensor shape (N, M)
+      Indices of M neighbors of each atom
+    crystal_atom_idx: list of torch.LongTensor of length N0
+      Mapping from the crystal idx to atom idx
+    target: tf.Tensor shape (N, 1)
+      Target value for prediction
+    batch_cif_ids: list
+    """
+    batch_atom_fea, batch_nbr_fea, batch_nbr_fea_idx = [], [], []
+    crystal_atom_idx, batch_target = [], []
+    batch_cif_ids = []
+    base_idx = 0
+    for i, ((atom_fea, nbr_fea, nbr_fea_idx), target, cif_id)\
+            in enumerate(dataset_list):
+        n_i = atom_fea.shape[0]  # number of atoms for this crystal
+        batch_atom_fea.append(atom_fea)
+        batch_nbr_fea.append(nbr_fea)
+        batch_nbr_fea_idx.append(nbr_fea_idx+base_idx)
+        new_idx = tf.constant(np.arange(n_i)+base_idx)
+        crystal_atom_idx.append(new_idx)
+        batch_target.append(target)
+        batch_cif_ids.append(cif_id)
+        base_idx += n_i
+    return (tf.concat(batch_atom_fea, axis=0),
+            tf.concat(batch_nbr_fea, axis=0),
+            tf.concat(batch_nbr_fea_idx, axis=0),
+            crystal_atom_idx),\
+        tf.stack(batch_target, axis=0),\
+        batch_cif_ids
+
+
+class GaussianDistance(object):
+    """
+    Expands the distance by Gaussian basis.
+
+    Unit: angstrom
+    """
+    def __init__(self, dmin, dmax, step, var=None):
+        """
+        Parameters
+        ----------
+
+        dmin: float
+          Minimum interatomic distance
+        dmax: float
+          Maximum interatomic distance
+        step: float
+          Step size for the Gaussian filter
+        """
+        assert dmin < dmax
+        assert dmax - dmin > step
+        self.filter = np.arange(dmin, dmax+step, step)
+        if var is None:
+            var = step
+        self.var = var
+
+    def expand(self, distances):
+        """
+        Apply Gaussian disntance filter to a numpy distance array
+
+        Parameters
+        ----------
+
+        distance: np.array shape n-d array
+          A distance matrix of any shape
+
+        Returns
+        -------
+        expanded_distance: shape (n+1)-d array
+          Expanded distance matrix with the last dimension of length
+          len(self.filter)
+        """
+        return np.exp(-(distances[..., np.newaxis] - self.filter)**2 /
+                      self.var**2)
+
+
+class AtomInitializer(object):
+    """
+    Base class for intializing the vector representation for atoms.
+
+    !!! Use one AtomInitializer per dataset !!!
+    """
+    def __init__(self, atom_types):
+        self.atom_types = set(atom_types)
+        self._embedding = {}
+
+    def get_atom_fea(self, atom_type):
+        assert atom_type in self.atom_types
+        return self._embedding[atom_type]
+
+    def load_state_dict(self, state_dict):
+        self._embedding = state_dict
+        self.atom_types = set(self._embedding.keys())
+        self._decodedict = {idx: atom_type for atom_type, idx in
+                            self._embedding.items()}
+
+    def state_dict(self):
+        return self._embedding
+
+    def decode(self, idx):
+        if not hasattr(self, '_decodedict'):
+            self._decodedict = {idx: atom_type for atom_type, idx in
+                                self._embedding.items()}
+        return self._decodedict[idx]
+
+
+class AtomCustomJSONInitializer(AtomInitializer):
+    """
+    Initialize atom feature vectors using a JSON file, which is a python
+    dictionary mapping from element number to a list representing the
+    feature vector of the element.
+
+    Parameters
+    ----------
+
+    elem_embedding_file: str
+        The path to the .json file
+    """
+    def __init__(self, elem_embedding_file):
+        with open(elem_embedding_file) as f:
+            elem_embedding = json.load(f)
+        elem_embedding = {int(key): value for key, value
+                          in elem_embedding.items()}
+        atom_types = set(elem_embedding.keys())
+        super(AtomCustomJSONInitializer, self).__init__(atom_types)
+        for key, value in elem_embedding.items():
+            self._embedding[key] = np.array(value, dtype=float)
+
+
+class CIFData(tf.keras.utils.Sequence):
+    """
+    The CIFData dataset is a wrapper for a dataset where the crystal structures
+    are stored in the form of CIF files. The dataset should have the following
+    directory structure:
+
+    root_dir
+    ├── id_prop.csv
+    ├── atom_init.json
+    ├── id0.cif
+    ├── id1.cif
+    ├── ...
+
+    id_prop.csv: a CSV file with two columns. The first column recodes a
+    unique ID for each crystal, and the second column recodes the value of
+    target property.
+
+    atom_init.json: a JSON file that stores the initialization vector for each
+    element.
+
+    ID.cif: a CIF file that recodes the crystal structure, where ID is the
+    unique ID for the crystal.
+
+    Parameters
+    ----------
+
+    root_dir: str
+        The path to the root directory of the dataset
+    max_num_nbr: int
+        The maximum number of neighbors while constructing the crystal graph
+    radius: float
+        The cutoff radius for searching neighbors
+    dmin: float
+        The minimum distance for constructing GaussianDistance
+    step: float
+        The step size for constructing GaussianDistance
+    random_seed: int
+        Random seed for shuffling the dataset
+
+    Returns
+    -------
+
+    atom_fea: tf.Tensor shape (n_i, atom_fea_len)
+    nbr_fea: tf.Tensor shape (n_i, M, nbr_fea_len)
+    nbr_fea_idx: torch.LongTensor shape (n_i, M)
+    target: tf.Tensor shape (1, )
+    cif_id: str or int
+    """
+    def __init__(self, root_dir, max_num_nbr=12, radius=8, dmin=0, step=0.2,
+                 random_seed=123):
+        self.root_dir = root_dir
+        self.max_num_nbr, self.radius = max_num_nbr, radius
+        assert os.path.exists(root_dir), 'root_dir does not exist!'
+        id_prop_file = os.path.join(self.root_dir, 'id_prop.csv')
+        assert os.path.exists(id_prop_file), 'id_prop.csv does not exist!'
+        with open(id_prop_file) as f:
+            reader = csv.reader(f)
+            self.id_prop_data = [row for row in reader]
+        random.seed(random_seed)
+        random.shuffle(self.id_prop_data)
+        atom_init_file = os.path.join(self.root_dir, 'atom_init.json')
+        assert os.path.exists(atom_init_file), 'atom_init.json does not exist!'
+        self.ari = AtomCustomJSONInitializer(atom_init_file)
+        self.gdf = GaussianDistance(dmin=dmin, dmax=self.radius, step=step)
+
+    def __len__(self):
+        return len(self.id_prop_data)
+
+    @functools.lru_cache(maxsize=None)  # Cache loaded structures
+    def __getitem__(self, idx):
+        cif_id, target = self.id_prop_data[idx]
+        crystal = Structure.from_file(os.path.join(self.root_dir,
+                                                   cif_id+'.cif'))
+        atom_fea = np.vstack([self.ari.get_atom_fea(crystal[i].specie.number)
+                              for i in range(len(crystal))])
+        atom_fea = tf.constant(atom_fea)
+        all_nbrs = crystal.get_all_neighbors(self.radius, include_index=True)
+        all_nbrs = [sorted(nbrs, key=lambda x: x[1]) for nbrs in all_nbrs]
+        nbr_fea_idx, nbr_fea = [], []
+        for nbr in all_nbrs:
+            if len(nbr) < self.max_num_nbr:
+                warnings.warn('{} not find enough neighbors to build graph. '
+                              'If it happens frequently, consider increase '
+                              'radius.'.format(cif_id))
+                nbr_fea_idx.append(list(map(lambda x: x[2], nbr)) +
+                                   [0] * (self.max_num_nbr - len(nbr)))
+                nbr_fea.append(list(map(lambda x: x[1], nbr)) +
+                               [self.radius + 1.] * (self.max_num_nbr -
+                                                     len(nbr)))
+            else:
+                nbr_fea_idx.append(list(map(lambda x: x[2],
+                                            nbr[:self.max_num_nbr])))
+                nbr_fea.append(list(map(lambda x: x[1],
+                                        nbr[:self.max_num_nbr])))
+        nbr_fea_idx, nbr_fea = np.array(nbr_fea_idx), np.array(nbr_fea)
+        nbr_fea = self.gdf.expand(nbr_fea)
+        atom_fea = tf.constant(atom_fea, dtype=tf.float64)
+        nbr_fea = tf.constant(nbr_fea, dtype=tf.float64)
+        nbr_fea_idx = tf.constant(nbr_fea_idx)
+        target = tf.constant([float(target)])
+        return (atom_fea, nbr_fea, nbr_fea_idx), target, cif_id
---
+++ cgcnn/model_tf.py
@@ -0,0 +1,184 @@
+from __future__ import print_function, division
+
+# import torch
+# import torch.nn as nn
+from tensorflow import keras
+import tensorflow as tf
+
+
+class ConvLayer(tf.keras.Model):
+    """
+    Convolutional operation on graphs
+    """
+    def __init__(self, atom_fea_len, nbr_fea_len):
+        """
+        Initialize ConvLayer.
+
+        Parameters
+        ----------
+
+        atom_fea_len: int
+          Number of atom hidden features.
+        nbr_fea_len: int
+          Number of bond features.
+        """
+        super(ConvLayer, self).__init__()
+        self.atom_fea_len = atom_fea_len
+        self.nbr_fea_len = nbr_fea_len
+        
+        # input size : 2*self.atom_fea_len+self.nbr_fea_len
+        self.fc_full = tf.keras.layers.Dense(2*self.atom_fea_len)
+        self.sigmoid = tf.keras.layers.Activation('sigmoid')
+        self.softplus1 = tf.keras.layers.Activation('softplus')
+        self.softplus2 = tf.keras.layers.Activation('softplus')
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        
+
+    def call(self, atom_in_fea, nbr_fea, nbr_fea_idx):
+#        input
+        print(nbr_fea_idx.shape)
+        (N, M) = nbr_fea_idx.shape
+#        # convolution
+        atom_nbr_fea = atom_in_fea[nbr_fea_idx, :]
+        total_nbr_fea = tf.concat(
+            [tf.broadcast_to(tf.expand_dims(atom_in_fea, axis=1), [N, M, self.atom_fea_len]),
+             atom_nbr_fea, nbr_fea], axis=2)
+        
+        keras
+        total_gated_fea = self.fc_full(total_nbr_fea)
+        total_gated_fea = tf.reshape(self.bn1(tf.reshape(total_gated_fea, [-1, self.atom_fea_len*2])), [N, M, self.atom_fea_len*2])
+    
+        nbr_filter, nbr_core =tf.split(total_gated_fea, num_or_size_splits=2, axis=2)
+        nbr_filter = self.sigmoid(nbr_filter)
+        nbr_core = self.softplus1(nbr_core)
+        nbr_sumed = tf.reduce_sum(nbr_filter * nbr_core, axis=1)
+        nbr_sumed = self.bn2(nbr_sumed)
+        out = self.softplus2(atom_in_fea + nbr_sumed)
+        return out
+        
+
+
+class CrystalGraphConvNet(tf.keras.Model):
+    """
+    Create a crystal graph convolutional neural network for predicting total
+    material properties.
+    """
+    def __init__(self, orig_atom_fea_len, nbr_fea_len,
+                 atom_fea_len=64, n_conv=3, h_fea_len=128, n_h=1,
+                 classification=False):
+        """
+        Initialize CrystalGraphConvNet.
+
+        Parameters
+        ----------
+
+        orig_atom_fea_len: int
+          Number of atom features in the input.
+        nbr_fea_len: int
+          Number of bond features.
+        atom_fea_len: int
+          Number of hidden atom features in the convolutional layers
+        n_conv: int
+          Number of convolutional layers
+        h_fea_len: int
+          Number of hidden features after pooling
+        n_h: int
+          Number of hidden layers after pooling
+        """
+        super(CrystalGraphConvNet, self).__init__()
+        
+        #self.fc_full = tf.keras.layers.Dense(2*self.atom_fea_len)
+        #self.sigmoid = tf.keras.layers.Activation('sigmoid')
+        #self.softplus1 = tf.keras.layers.Activation('softplus')
+        #self.softplus2 = tf.keras.layers.Activation('softplus')
+        #self.bn1 = tf.keras.layers.BatchNormalization()
+        #self.bn2 = tf.keras.layers.BatchNormalization()
+        
+        self.classification = classification
+        self.embedding = tf.keras.layers.Dense(atom_fea_len)
+        self.convs = [ConvLayer(atom_fea_len=atom_fea_len,
+                                    nbr_fea_len=nbr_fea_len)
+                                    for _ in range(n_conv)]
+        self.conv_to_fc = tf.keras.layers.Dense(h_fea_len)
+        self.conv_to_fc_softplus = tf.keras.layers.Activation('softplus')
+        if n_h > 1:
+            self.fcs = [tf.keras.layers.Dense(h_fea_len, h_fea_len) for _ in range(n_h-1)]
+            self.softpluses = [tf.keras.layers.Activation('softplus') for _ in range(n_h-1)]
+        if self.classification:
+            self.fc_out = tf.keras.layers.Dense(2)
+        else:
+            self.fc_out = tf.keras.layers.Dense(1)
+        if self.classification:
+            self.logsoftmax = tf.nn.log_softmax
+            self.dropout = tf.keras.layers.Dropout(rate=0.5)
+            #self.logsoftmax = tf.nn.log_softmax(dim=1)
+            #self.dropout = nn.Dropout()
+
+    def call(self, atom_fea, nbr_fea, nbr_fea_idx, crystal_atom_idx):
+        """
+        Forward pass
+
+        N: Total number of atoms in the batch
+        M: Max number of neighbors
+        N0: Total number of crystals in the batch
+
+        Parameters
+        ----------
+
+        atom_fea: Variable(torch.Tensor) shape (N, orig_atom_fea_len)
+          Atom features from atom type
+        nbr_fea: Variable(torch.Tensor) shape (N, M, nbr_fea_len)
+          Bond features of each atom's M neighbors
+        nbr_fea_idx: torch.LongTensor shape (N, M)
+          Indices of M neighbors of each atom
+        crystal_atom_idx: list of torch.LongTensor of length N0
+          Mapping from the crystal idx to atom idx
+
+        Returns
+        -------
+
+        prediction: nn.Variable shape (N, )
+          Atom hidden features after convolution
+
+        """
+        #atom_fea, nbr_fea, nbr_fea_idx, crystal_atom_idx = inputs[0], inputs[1], tf.cast(inputs[2], tf.int32), tf.cast(inputs[3], tf.int32)
+
+        print("atom_fea_len", len(atom_fea))
+        print("nbr_fea_len", len(nbr_fea))
+        print("nbr_fea_idx_len", len(nbr_fea_idx))
+        atom_fea = self.embedding(atom_fea)
+        for conv_func in self.convs:
+            atom_fea = conv_func(atom_fea, nbr_fea, nbr_fea_idx)
+        crys_fea = self.pooling(atom_fea, crystal_atom_idx)
+        crys_fea = self.conv_to_fc(self.conv_to_fc_softplus(crys_fea))
+        crys_fea = self.conv_to_fc_softplus(crys_fea)
+        if self.classification:
+            crys_fea = self.dropout(crys_fea)
+        if hasattr(self, 'fcs') and hasattr(self, 'softpluses'):
+            for fc, softplus in zip(self.fcs, self.softpluses):
+                crys_fea = softplus(fc(crys_fea))
+        out = self.fc_out(crys_fea)
+        if self.classification:
+            out = self.logsoftmax(out, axis=1)
+        return out
+
+    def pooling(self, atom_fea, crystal_atom_idx):
+        """
+        Pooling the atom features to crystal features
+
+        N: Total number of atoms in the batch
+        N0: Total number of crystals in the batch
+
+        Parameters
+        ----------
+
+        atom_fea: Variable(torch.Tensor) shape (N, atom_fea_len)
+          Atom feature vectors of the batch
+        crystal_atom_idx: list of torch.LongTensor of length N0
+          Mapping from the crystal idx to atom idx
+        """
+        assert sum([len(idx_map) for idx_map in crystal_atom_idx]) == atom_fea.shape[0]
+        summed_fea = [tf.math.reduce_mean(atom_fea[idx_map], axis=0, keepdims=True)
+                      for idx_map in crystal_atom_idx]
+        return tf.concat(summed_fea, axis=0)
